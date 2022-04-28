3483773064f45452e4415f4e0a0a896e3fe26392
nyinyilayhtet <nnlayhtet@gmail.com>
2022-04-28 17:12:06 -0400

    commit tree 42ee2dd71be30c2cd8d514cac1cc4ea48ed05249

--- index.js
+++ index.js
@@ -12,5 +12,38 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
-}
+  return tutorials.map((element) => {
+    const wordArray = element.split(" ")
+    const capWordArray = wordArray.map((word)=>word.charAt(0).toUpperCase()+word.slice(1))
+    const joinWords = capWordArray.join(" ")
+    return joinWords;
+  });
+};
+console.log(titleCased())
+
+
+
+
+
+
+
+
+// BELOW IS SOLUTION POSTED
+// const titleCased = () => {
+//   return tutorials.map((line) => {
+//     const tokens = line.split(" ");
+//     const capitalizedTokens = tokens.map((token) =>token.charAt(0).toUpperCase() + token.slice(1));
+//     const response = capitalizedTokens.join(" ");
+//     return response;
+//   });
+// };
+
+// ## code below turns the first letter of string to Cap ##
+// const myStr = "this is a sample string. hi there!"
+// function doCap(inputString){
+//     const myArray = inputString.split(" ")
+//     console.log(myArray)
+//     const capString = myArray.map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+// return capString.join(" ")
+// }
+// console.log(doCap(myStr));
